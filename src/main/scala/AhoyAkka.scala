6fefd93bdfcfbc108a41479bad8f216809c8653d
kmehkeri <kmehkeri@gmail.com>
2016-09-18 18:14:53 +0000

    Use App trait on AhoyAkka class

--- src/main/scala/AhoyAkka.scala
+++ src/main/scala/AhoyAkka.scala
@@ -6,53 +6,51 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 
-object AhoyAkka {
-  def main(args: Array[String]) {
-    val host = "localhost"
-    val port = 8888
+object AhoyAkka extends App {
+  val host = "localhost"
+  val port = 8888
 
-    implicit val actorSystem = ActorSystem("ahoy-system")
-    implicit val actorMaterializer = ActorMaterializer()
-    implicit val executionContext = actorSystem.dispatcher
-    implicit val taskJsonFormat = jsonFormat2(Task)
+  implicit val actorSystem = ActorSystem("ahoy-system")
+  implicit val actorMaterializer = ActorMaterializer()
+  implicit val executionContext = actorSystem.dispatcher
+  implicit val taskJsonFormat = jsonFormat2(Task)
 
-    val route =
-      get {
-        pathSingleSlash {
-          val statusF = Service.status
-          onSuccess(statusF) {
-            case status => complete(status)
-          }
-        } ~
-        path("task" / IntNumber) { id =>
-          val taskF = Service.getTask(id)
-          onSuccess(taskF) {
-            case Left(msg) => complete(Map("error" -> msg))
-            case Right(task) => complete(task)
-          }
+  val route =
+    get {
+      pathSingleSlash {
+        val statusF = Service.status
+        onSuccess(statusF) {
+          case status => complete(status)
         }
       } ~
-      post {
-        path("submit") {
-          fileUpload("file") {
-            case (metadata, byteStream) =>
-              complete {
-                StatusCodes.Accepted -> Task(1, "Processing")
-              }
-          }
+      path("task" / IntNumber) { id =>
+        val taskF = Service.getTask(id)
+        onSuccess(taskF) {
+          case Left(msg) => complete(Map("error" -> msg))
+          case Right(task) => complete(task)
         }
       }
-
-    val bindingFuture = Http().bindAndHandle(route, host, port)
-
-    bindingFuture.map { b =>
-      println(s"Server started at ${host}:${port}\nPress ENTER to stop...")
-      readLine()
-      b
-    }.flatMap(_.unbind()).onComplete { _ =>
-      println("Terminating...")
-      actorSystem.terminate()
+    } ~
+    post {
+      path("submit") {
+        fileUpload("file") {
+          case (metadata, byteStream) =>
+            complete {
+              StatusCodes.Accepted -> Task(1, "Processing")
+            }
+        }
+      }
     }
+
+  val bindingFuture = Http().bindAndHandle(route, host, port)
+
+  bindingFuture.map { b =>
+    println(s"Server started at ${host}:${port}\nPress ENTER to stop...")
+    readLine()
+    b
+  }.flatMap(_.unbind()).onComplete { _ =>
+    println("Terminating...")
+    actorSystem.terminate()
   }
 }
 
